9089ea2b5b62612591a86db78de2f1ce94828b7a
James Fenton <james_e_fenton@hotmail.com>
2016-03-02 16:59:45 +0000

    This sends and receives to messages OK

--- coms/simple/test_coms_crc_1.py
+++ coms/simple/test_coms_crc_1.py
@@ -1,6 +1,6 @@
 import serial
 import time
-
+import minimalmodbus
 
 # these are allowed
 # ser = serial.Serial("COM9", 9600, bytesize=8, parity='N', stopbits=1,timeout=1, xonxoff=0, rtscts=0)
@@ -8,27 +8,49 @@ import time
 # ser = serial.Serial("COM9", 9600, bytesize=7, parity='O', stopbits=1,timeout=1, xonxoff=0, rtscts=0)
 
 try:
-    ser = serial.Serial("COM9", 19200, bytesize=7, parity='O', stopbits=1, timeout=0, xonxoff=0, rtscts=0)
+    ser = serial.Serial("COM9", 19200, bytesize=8, parity='O', stopbits=1, timeout=0, xonxoff=0, rtscts=0)
     # open the serial port
 except:
     print('Can''t open com port')
     raw_input("Press Enter to exit")
     exit()
 
-test_message = "1234567890\n"    
+test_message = "123456789012345"    
 if ser.isOpen():
     print(ser.name + ' is open...')
-    print('Try and write the following 10 chars:' + test_message)
+    print('Try and write the following ' + str(len(test_message)) + ' chars:' + test_message)
     ser.write(test_message)
     time.sleep(1)
     #read_message = ser.read(10)
     read_message = ""
     while ser.inWaiting() > 0:
         read_message += ser.read(1)
-    print('Read the following back:' + read_message)
-    
+    print('Read the following ' + str(len(read_message)) + ' characters back:' + read_message)
+    time.sleep(1)
+    test_message = "abcdefghijklmno"
+    print('Try and write the following ' + str(len(test_message)) + ' chars:' + test_message)
+    ser.write(test_message)
+    time.sleep(1)
+    #read_message = ser.read(10)
+    read_message = ""
+    while ser.inWaiting() > 0:
+        read_message += ser.read(1)
+    print('Read the following ' + str(len(read_message)) + ' characters back:' + read_message)    
 ser.close()
 
+time.sleep(2)
+try:
+    instrument = minimalmodbus.Instrument('COM9', 1)
+    instrument.debug = True
+    instrument.mode = minimalmodbus.MODE_ASCII
+except:
+    print("Can't create instrument")
+
+try:
+    temperature = instrument.read_register(289, 1)
+except:
+    print("Can't read instrument")
 
         
-
+# if I set bytesize=8
+# must set UartHandle.Init.WordLength   = UART_WORDLENGTH_9B; on ST
